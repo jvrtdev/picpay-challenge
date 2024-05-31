acfbd4c4b4d5b2b9cfa707bb74e274f2f74ecf2a
jvrtdev-linux <joaovitorromanoteixeira@gmail.com>
2024-05-31 14:16:03 -0300

    :sparkles: feat: add hash on password

--- backend/src/app/controllers/UserController.ts
+++ backend/src/app/controllers/UserController.ts
@@ -2,14 +2,22 @@ import { Request, Response } from "express";
 import UserService from "../service/UserService";
 import WalletService from "../service/WalletService";
 import { generateToken } from "../functions/jwt";
+import { CreateAccountTypes } from "../service/@types";
+import { comparePassword, hashPassword } from "../functions/password-hash";
 
 
 
 class UserController {
 
   async CreateAccount(req: Request, res: Response){
-    const data = req.body;
-    
+    const data: CreateAccountTypes = {
+      name: req.body.name,
+      email: req.body.email,
+      document_number: req.body.document_number,
+      document_type: req.body.document_type,
+      password: await hashPassword(req.body.password),
+    }
+
     try {
       const newUser = await UserService.createAccount(data);
 
@@ -32,17 +40,18 @@ class UserController {
     const data = req.body;
 
     try{
-      const findUser = await UserService.loginUser(data.login, data.password);
-      
-      if(findUser !== null){
+      const findUser = await UserService.loginUser(data.login);
+      const isMatch = await comparePassword(data.password, findUser?.password?? '')
+
+      if(isMatch){
         const payload = {id: findUser?.id, docnumber: findUser?.documentNumber, password: findUser?.password}
         const newtoken =  generateToken(payload);
-        return res.status(202).setHeader('Authorization', newtoken).json(findUser);
+        return res.status(202).setHeader('Authorization', newtoken).json(newtoken);
       }
-      return res.status(401).send("Login ou senha incorretos ou inexistentes")
+      return res.status(401).json("Login ou senha incorretos ou inexistentes");
 
     } catch(error) {
-      return res.status(400).json(error)
+      return res.status(400).json({"Erro" : `${error}`})
     }
 
   }
